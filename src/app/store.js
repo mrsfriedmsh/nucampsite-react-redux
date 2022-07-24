182a6f971d1ac68ca0c3407793d415d4981d1573
mrsfriedmsh <99189153+mrsfriedmsh@users.noreply.github.com>
2022-07-24 11:04:40 -0700

    Nucampsite react/redux final build

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import logger from 'redux-logger';
+import { campsitesReducer } from '../features/campsites/campsitesSlice';
+import { commentsReducer } from '../features/comments/commentsSlice';
+import { promotionsReducer } from '../features/promotions/promotionsSlice';
+import { partnersReducer } from '../features/PARTNERS/partnersSlice';
+import { userReducer } from '../features/user/userSlice';
 
-export const store = configureStore({
+export const store = configureStore ({
   reducer: {
-    counter: counterReducer,
+    campsites: campsitesReducer,
+    comments: commentsReducer,
+    promotions: promotionsReducer,
+    partners: partnersReducer,
+    user: userReducer
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
---
+++ src/features/PARTNERS/PartnersList.js
@@ -0,0 +1,35 @@
+import { useSelector } from 'react-redux';
+import { Row, Col } from 'reactstrap';
+import Partner from './Partner';
+import { selectAllPartners } from './partnersSlice';
+import Loading from '../../components/Loading';
+import Error from '../../components/Error';
+
+ const PartnersList = () => { 
+    const partners = useSelector(selectAllPartners);
+
+    const isLoading = useSelector((state) => state.partners.isLoading);
+    const errMsg = useSelector((state) => state.partners.errMsg);
+
+    //ternary operator below
+
+    return isLoading ? (
+        <Loading />
+    ) : errMsg ? (
+        <Error errMsg={errMsg} />
+    ) : (
+        <Col className='mt-4'>
+            <Row>
+                {partners.map((partner) => {
+                    return ( 
+                        <div className='d-flex mb-5' key={partner.id}>
+                            <Partner partner={partner} />
+                        </div>
+                    );
+                })}
+            </Row>
+        </Col>
+    );
+ };
+
+ export default PartnersList;
